5b601c484d86c02454a82934c02c8ba546de4d0b
sgaete <gaetevsa@gmail.com>
2016-09-01 04:27:25 -0300

    se agrega funcionalidades a ctrl_main

--- ctrl_main.py
+++ ctrl_main.py
@@ -3,7 +3,10 @@
 import sys
 from PySide import QtGui
 from ui_main import Ui_Main
+#from cliente.ctrl_cliente import Widget_cliente
+from marca.ctrl_marca import Widget_marca
 from modelo.ctrl_modelo import Widget_modelo
+#from venta.ctrl_venta import Widget_venta
 
 class Main(QtGui.QMainWindow):
     """
@@ -18,24 +21,34 @@ class Main(QtGui.QMainWindow):
         self.show()
 
     def menu_actions(self):
-        self.ui.actionModelos.triggered.connect(self.load_modelo)
-        self.ui.actionMarcas.triggered.connect(self.load_marca)
-        self.ui.actionClientes.triggered.connect(self.load_cliente)
-        self.ui.actionVentas.triggered.connect(self.load_venta)
         
-    def load_modelo(self):
-        widget = Widget_modelo(self)
-        self.setCentralWidget(widget)
-    def load_marca(self):
-        widget = Widget_marca(self)
-        self.setCentralWidget(widget)
+        self.ui.actionClientes.triggered.connect(self.load_cliente)
+        self.ui.actionMarcas.triggered.connect(self.load_marca)
+        self.ui.actionModelos.triggered.connect(self.load_modelo)
+        self.ui.actionVentas.triggered.connect(self.load_venta)
+
+        self.ui.actionSalir.triggered.connect(self.cerrar)
+        
+    
+    def cerrar(self):
+        exit()
+        
     def load_cliente(self):
         widget = Widget_cliente(self)
         self.setCentralWidget(widget)
+
+    def load_marca(self):
+        widget = Widget_marca(self)
+        self.setCentralWidget(widget)
+
+    def load_modelo(self):
+        widget = Widget_modelo(self)
+        self.setCentralWidget(widget)
+
     def load_venta(self):
         widget = Widget_venta(self)
         self.setCentralWidget(widget)
-        
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     main = Main()
